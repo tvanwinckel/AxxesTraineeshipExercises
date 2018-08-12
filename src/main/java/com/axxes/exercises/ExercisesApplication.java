dd71d9496d35c11c4856d9a59aa296304eb133fe
Thomas Van Winckel <vanwinckelthomas@gmail.com>
2018-08-12 12:39:22 +0200

    Initial commit.

---
+++ src/main/java/com/axxes/exercises/ExercisesApplication.java
@@ -0,0 +1,12 @@
+package com.axxes.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesApplication.class, args);
+	}
+}
