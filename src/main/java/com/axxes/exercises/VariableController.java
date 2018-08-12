4615194b68c5204ad38eff87ccb92e1eccf9ddb7
Thomas Van Winckel <vanwinckelthomas@gmail.com>
2018-08-12 13:50:41 +0200

    Exersice 4 : Solution

--- src/main/java/com/axxes/exercises/VariableController.java
+++ src/main/java/com/axxes/exercises/VariableController.java
@@ -1,4 +1,33 @@
 package com.axxes.exercises;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class VariableController {
+
+    @PostMapping("/string")
+    public String stringMapping(@RequestParam(name = "param", required = false, defaultValue = "DEFAULT") final String param,
+                                final Model model) {
+        model.addAttribute("message", "The request parameter is: " + param);
+        return "requestMappingView";
+    }
+
+    @PostMapping("/int")
+    public String intMapping(@RequestParam(name = "param") final int param,
+                             final Model model) {
+        model.addAttribute("message", "The request parameter is: " + param);
+        return "requestMappingView";
+    }
+
+    @GetMapping("/{int}")
+    public String postMapping(@PathVariable(name = "int") final int param,
+                              final Model model) {
+        model.addAttribute("message", "The request parameter is: " + param);
+        return "requestMappingView";
+    }
 }
