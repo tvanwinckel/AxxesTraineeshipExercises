f7ece0259de5101eca9c103fbbf546f0276e7669
Thomas Van Winckel <vanwinckelthomas@gmail.com>
2018-08-12 15:06:39 +0200

    Exersice 7 : Solution Extra

--- src/main/java/com/axxes/exercises/ExceptionController.java
+++ src/main/java/com/axxes/exercises/ExceptionController.java
@@ -1,22 +1,24 @@
 package com.axxes.exercises;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExceptionController {
 
     @ExceptionHandler(Exception.class)
-    public ModelAndView handleAll(final Exception e) {
-        final ModelAndView view = new ModelAndView("exceptionView");
-        view.addObject("message", "An exception occurred.");
-        return view;
+    public ResponseEntity<String> handleAll(final Exception e) {
+        return new ResponseEntity<>("A general error occured", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ModelAndView handleSpecific(final Exception e) {
         final ModelAndView view = new ModelAndView("exceptionView");
         view.addObject("message", "An IllegalArgument occurred.");
